f203d9085ff2c55e149f44cc78126a4fcf0a450c
Boban Sremcevic <bsremcevic@itekako.com>
2018-06-05 17:02:04 +0200

    Completed large hero clock and intro content block

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+require('./gulp/tasks/styles');
+require('./gulp/tasks/watch');
+
+// gulp.task('default', function() {
+//   console.log("Hooray - you created a Gulp task!");
+// });
+//
+// gulp.task('html', function() {
+//   console.log("Imagine something useful done here");
+// });
